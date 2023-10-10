819c9e6eec204805c37d26d3624482ba9783c0df
eiivanov20 <eiivanov20@codingburgas.bg>
2023-10-10 11:33:20 +0300

    10/10/23

---
+++ classWork2023/src/zad3/Main.java
@@ -0,0 +1,7 @@
+package zad3;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("1 2 3 4 5");
+    }
+}
---
+++ classWork2023/src/zad4/Main.java
@@ -0,0 +1,9 @@
+package zad4;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("1 2 3 4 5");
+        System.out.println("1 2 3 4 5");
+        System.out.println("1 2 3 4 5");
+    }
+}
